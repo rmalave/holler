beec8f76976cc6082309848f074da815050038fd
Rafael Malave <rafomalave@gmail.com>
2015-03-10 15:48:47 -0700

    Set up figaro on heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ CarrierWave.configure do |config|
         provider:'AWS',
         aws_access_key_id: ENV['aws_access_key_id'],
         aws_secret_access_key: ENV['aws_secret_access_key'],
-        host:'https://s3-eu-west-1.amazonaws.com/hollerreview'
     }
     config.fog_directory  = ENV['fog_directory'] 
 end
